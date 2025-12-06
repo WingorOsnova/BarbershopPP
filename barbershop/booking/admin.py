28c2d36a4878ae693d3fca9f92c2fa0fd256e36a
WingorOsnova <magilazgood@gmail.com>
2025-12-06 18:15:31 +0100

    Register Barber, Service, Booking in admin and add list_display, filters, search_fields, ordering

--- barbershop/booking/admin.py
+++ barbershop/booking/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Barber, Service, Booking
 
-# Register your models here.
+@admin.register(Barber)
+class AdminBarber(admin.ModelAdmin):
+  list_display = ('name', 'experience_years', 'is_active')
+  list_filter = ('is_active',)
+  search_fields = ('name',)
+
+@admin.register(Service)
+class AdminService(admin.ModelAdmin):
+  list_display = ('name', 'price', 'duration_minutes')
+  search_fields = ('name',)
+
+@admin.register(Booking)
+class AdminBooking(admin.ModelAdmin):
+  list_display = (
+    'client_name',
+    'client_phone',
+    'barber',
+    'service',
+    'booking_date',
+    'booking_time',
+    'status',
+    'created_at'
+  )
+  list_filter = ('status', 'barber', 'service', 'booking_date')
+  search_fields = ('client_name', 'client_phone','client_email')
+  ordering = ('-booking_time', '-booking_date')
+  
--- barbershop/booking/models.py
+++ barbershop/booking/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Barber(models.Model):
+  name = models.CharField(max_length=100)
+  photo_url = models.URLField(blank=True)
+  experience_years = models.PositiveIntegerField(default=0)
+  description = models.TextField(blank=True)
+  is_active = models.BooleanField(default=True)
+  
+  def __str__(self):
+    return self.name
+
+class Service(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(blank=True)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  duration_minutes = models.PositiveIntegerField(default=30)
+
+  def __str__(self):
+    return self.name
+
+class Booking(models.Model):
+  client_name = models.CharField(max_length=100)
+  client_phone = models.CharField(max_length=20)
+  client_email = models.EmailField(blank=True)
+  barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
+  service = models.ForeignKey(Service, on_delete=models.CASCADE)
+  booking_date = models.DateField()
+  booking_time = models.TimeField()
+  message = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  STATUS_PENDING = 'pending'
+  STATUS_CONFIRMED = 'confirmed'
+  STATUS_CANCELED = 'canceled'
+  STATUS_COMPLETED = 'completed'
+
+  STATUS_CHOICES = [
+    (STATUS_PENDING, 'В ожидание'),
+    (STATUS_CONFIRMED, 'Подтвержденно'),
+    (STATUS_CANCELED, 'Отмененно'),
+    (STATUS_COMPLETED, 'Выполненно'),
+  ]
+
+  status = models.CharField(
+    max_length=20,
+    choices=STATUS_CHOICES,
+    default=STATUS_PENDING,
+    verbose_name='Статус'
+  )
+
+  def __str__(self):
+    return f"{self.client_name} - {self.barber.name} - {self.service.name} - {self.booking_date}"
